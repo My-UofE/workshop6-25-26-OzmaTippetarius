bafb6fb44b99e18f36889c673ee22306db91fb12
Kade <eh1012@exeter.ac.uk>
2026-02-20 13:18:43 +0000

    competed

--- RPSApp.java
+++ RPSApp.java
@@ -19,11 +19,16 @@ public class RPSApp {
 
         switch (n){
             case 0:
-                HandSign = HandSign.ROCK;
+                computerMove = HandSign.ROCK;
+                break;
             case 1:
-                HandSign = HandSign.PAPER;
+                computerMove = HandSign.PAPER;
+                break;
             case 2:
-                HandSign = HandSign.SCISSORS;
+                computerMove = HandSign.SCISSORS;
+                break;
+            default:
+                computerMove = null;
         }
 
         return computerMove;
@@ -37,21 +42,51 @@ public class RPSApp {
         Scanner in = new Scanner(System.in);
         // Use a variable to tag if the input is valid 
         // (one of the characters {s,S,p,P,r,R,q,Q}) or not
-        boolean validInput = true;
+        boolean validInput = false;
         HandSign playerHandSign = null;
         do {// repeat until valid input
 
             // Add your code to give some description about what input the
             //  users are supposed to give
-            System.out.println("");
+            System.out.println("Enter R for rock, P for paper or S for scissors (or Q to quit)");
 
             // convert the input string into a char type
             char inChar = in.next().toLowerCase().charAt(0);
 
-            // Add your code to output the player’s hand sign. A
-            //switch-statement is a good choice.
-        
-            ...
+            switch (inChar){
+                case 's':
+                    playerHandSign = HandSign.SCISSORS;
+                    validInput = true;
+                    break;
+                case 'S':
+                    playerHandSign = HandSign.SCISSORS;
+                    validInput = true;
+                    break;
+                case 'r':
+                    playerHandSign = HandSign.ROCK;
+                    validInput = true;
+                    break;
+                case 'R':
+                    playerHandSign = HandSign.ROCK;
+                    validInput = true;
+                    break;
+                case 'p':
+                    playerHandSign = HandSign.PAPER;
+                    validInput = true;
+                    break;
+                case 'P':
+                    playerHandSign = HandSign.PAPER;
+                    validInput = true;
+                    break;
+                case 'q':
+                    validInput = true;
+                    break;
+                case 'Q' :
+                    validInput = true;
+                    break;
+                default:
+                    playerHandSign = null;
+            }
 
 
         } while(!validInput);
@@ -71,7 +106,13 @@ public class RPSApp {
      *
      */
     public static int whoWins(HandSign h1, HandSign h2){
-         ...
+         if (h1 == h2){
+            return 0;
+         } else if ((h1 == HandSign.ROCK && h2 == HandSign.SCISSORS) || (h1 == HandSign.PAPER && h2 == HandSign.ROCK) || h1== HandSign.SCISSORS && h2 == HandSign.PAPER){
+            return 1;
+         } else {
+            return -1;
+         }
     }
     
     /**
@@ -89,15 +130,34 @@ public class RPSApp {
         while (!gameOver){
             // repeat this process till the user quits
             
-            //Step1: Get the player move from the keyboard input
+        playerMove = getPlayerMove();
+        if (playerMove == null){
+            gameOver = true;
+            continue;
+        }
 
-            //Step2: Get the computer’s move (randomly generated)
+        computerMove = getComputerMove();
 
-            //Step3: Check who wins
+        checkwin= whoWins(playerMove, computerMove);
 
-            //Step4: Output who played what and who won the round
+        System.out.println ("Player played : "+ playerMove);
+        System.out.println ("Computer played : "+ computerMove);
+        switch (checkwin) {
+            case 1:
+                System.out.println ("Player won!");
+                playerScore += 1;
+                break;
+            case -1:
+                System.out.println ("Computer won!");
+                computerScore += 1;
+                break;
+            default:
+                System.out.println ("It's a tie!");
 
-            //Step5: Update and print player/computer scores
+        }
+        
+        System.out.println ("Player score : "+ playerScore);
+        System.out.println ("Computer score : "+ computerScore);
 
         }
     }
